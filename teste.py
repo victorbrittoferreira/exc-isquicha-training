995db894a79f79b928d2cbbd109195ad2813828d
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-06-03 17:18:29 -0300

    EXC 23

--- teste.py
+++ teste.py
@@ -495,3 +495,39 @@
 ## 21 Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 #Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 
+#sex =  (input('Digite seu genêro B I O L Ó G I C O: '))
+#
+#if sex == 'm' or sex == 'M':
+#    print(f'{sex} é masculino')
+#
+#elif sex == 'f' or sex == 'F':
+#    print(f'{sex} é feminino')
+#    
+#else:
+#    print(f'{sex} é não um gênero válido')
+
+
+##22 Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+#letter =  input('Digite uma letra: ')
+#
+#vowel = ['a', 'e', 'i', 'o', 'u']
+#vowel_u = [v.upper() for v in vowel]
+#
+#
+#if (letter in vowel) or (letter in vowel_u):
+#    print (f'A letra {letter.upper()} é vogal')
+#
+#else:
+#    print (f'A letra {letter.upper()} é consoante')
+
+
+## 23 Faça um programa para a leitura de duas notas parciais de um aluno.
+#O programa deve calcular a média alcançada por aluno e apresentar:
+#    A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+#    A mensagem "Reprovado", se a média for menor do que sete;
+#    A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+
+
+
