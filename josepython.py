2c5ab25ed71419941ebb14a824345621cd4baf87
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-07-01 09:46:16 -0300

    strings editing

--- josepython.py
+++ josepython.py
@@ -5,16 +5,41 @@
 #print (x[::-1])
 
 
-x = 'Hello world'
-y = 'Hi this is a string'
+#x = 'Hello world'
+#y = 'Hi this is a string'
 ## slice
 #print (x[8])
 
 ## upper case
-print(x.upper())
+#print(x.upper())
 
 ## create a list
-print(x.split())
+#print(x.split())
 
 ## create another list, but considering a paremetter
-print(y.split('i'))
+#print(y.split('i'))
+
+## ways to format a string in
+
+#1
+#print ('The {0} {1} {2}'. format ('fox', 'brown', 'quick'))
+
+#2
+#print ('The {f} {f} {f}'. format (f = 'fox', b = 'brown', q = 'quick'))
+
+#3
+#animal = "fox"
+#print ('The {a} {a} {a}'. format (a = animal))
+
+#4
+#result = 13.5534534
+#print('Resultado {r: 5.2f}'.format (r = result))
+
+## %s  another way inject string into print statement 
+#print("Resultado  %s " %' foi esse')
+
+#multiple itens
+#print("Resultado  %s e %s" %('A', 'B'))
+ 
+print("Resultado  %s e %s" %('A', 'B'))
+ 
