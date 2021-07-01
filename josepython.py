0ce30689e969d37b337f429e3f2edcf29c751f4b
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-06-30 21:36:33 -0300

    jose portilla

---
+++ josepython.py
@@ -0,0 +1,20 @@
+## a trick to reverse a string
+
+#x = 'atecubanos'
+ 
+#print (x[::-1])
+
+
+x = 'Hello world'
+y = 'Hi this is a string'
+## slice
+#print (x[8])
+
+## upper case
+print(x.upper())
+
+## create a list
+print(x.split())
+
+## create another list, but considering a paremetter
+print(y.split('i'))
