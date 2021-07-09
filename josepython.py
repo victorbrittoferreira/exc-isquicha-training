af498b93475972f21ef856ab9b0cc48758076242
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-07-09 19:13:48 -0300

    grab test

--- josepython.py
+++ josepython.py
@@ -45,7 +45,96 @@
 #a , b = 'A' , 'B'
 #print("Resultado  %s e %s" %( b, a ))
 
-a = 1.2
 
-2 = 2.1
- 
+### FIRST ASSESTMENT
+
+#Answer the following questions
+#
+#Write (or just say out loud to yourself) a brief description of all the following Object Types and Data Structures we've learned about.
+# Really this is just to test if you know the difference between these, so feel free to just think about it, since your answers are self-graded.
+#
+#Numbers:
+#
+#Strings:
+#
+#Lists:
+#
+#Tuples:
+#
+#Dictionaries:
+#Numbers
+#
+#Strings
+#
+#Given the string 'hello' give an index command that returns 'e'. Enter your code in the cell below:
+#In [ ]:
+#
+## Print out 'e' using indexing
+#s = 'hello'
+#print(s[1])
+#print(s[0]+s[2:])
+
+#Reverse the string 'hello' using slicing:
+#print(s[::-1])
+
+#Lists
+#
+#Build this list [0,0,0] two separate ways.
+#x = [0,0,0]
+
+## Method 1:
+#
+#print(x.list()) 
+#
+## Method 2:
+#
+#Reassign 'hello' in this nested list to say 'goodbye' instead:
+#x[2] = 'hello'
+#
+#list3 = [1,2,[3,4,'hello']]
+#list3[2][2] = 'goodbye'
+#print(list3)
+
+#Sort the list below:
+#list4 = [5,3,4,6,1]
+#list4.sort()
+#print(list4)
+
+#Dictionaries
+#
+#Using keys and indexing, grab the 'hello' from the following dictionaries:
+#In [ ]:
+#
+#d = {'simple_key':'hello'}
+#Grab 'hello'
+#print(d['simple_key'])
+
+#d = {'k1':{'k2':'hello'}}
+## Grab 'hello'
+#print(d['k1']['k2'])
+
+## Getting a little tricker
+d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+##Grab hello
+#print(d['k1'][0]['nest_key'][1][0])
+
+## This will be hard and annoying!
+#d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+#
+#Can you sort a dictionary? Why or why not?
+#
+#Tuples
+#
+#What is the major difference between tuples and lists?
+#
+#How do you create a tuple?
+#
+#Sets
+#
+#What is unique about a set?
+#
+#Use a set to find the unique values of the list below:
+#In [ ]:
+#
+#list5 = [1,2,2,33,4,4,11,22,3,3,2]
+
